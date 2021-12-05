88d58b33f308462f8d31df2b440c06868afe08d0
Dante Mogrim <1991.mogrim@gmail.com>
2021-12-05 21:21:13 +0100

    Reverting RSS changes.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 import getRoutes from './utils/getRoutes';
-import getFeed from './utils/getFeed';
 import getSiteMeta from './utils/getSiteMeta';
 
 const meta = getSiteMeta();
@@ -116,9 +115,41 @@ export default {
     },
   },
 
-  feed: {
-    create() {
-      return getFeed();
+  feed: [
+    {
+      create: async (feed) => {
+        const $content = require('@nuxt/content').$content;
+        feed.options = {
+          title: 'dante.wiki',
+          link: 'https://www.dante.wiki/feed.xml',
+          description:
+            'My personal wikipedia filled with web dev related content, notes and guides.',
+          favicon: 'https://www.dante.wiki/icon.png',
+        };
+
+        const posts = await $content('posts')
+          .sortBy('gitUpdatedAt', 'desc')
+          .fetch();
+        posts.forEach((post) => {
+          const url = `https://www.dante.wiki/posts/${post.slug}`;
+          feed.addItem({
+            id: url,
+            title: post.title,
+            description: post.description,
+            date: new Date(post.gitUpdatedAt),
+            content: post.bodyHtml,
+            link: url,
+            author: {
+              name: 'Dante Mogrim',
+              email: '1991.mogrim@gmail.com',
+              link: 'https://www.dante.wiki',
+            },
+          });
+        });
+      },
+      path: '/feed.xml',
+      type: 'rss2',
+      data: ['posts', 'xml'],
     },
-  },
+  ],
 };
