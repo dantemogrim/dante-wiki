4e0ff5bec6fe9e3cf7629aa0a42f50b38a11f740
Dante Mogrim <66732473+dantemogrim@users.noreply.github.com>
2022-02-02 19:32:42 +0100

    Trying new things with an .ico.

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
 
     link: [
       // Favicon.
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       {
         // Canonical.
         rel: 'icon',
@@ -122,7 +122,7 @@ export default {
           link: 'https://www.dante.wiki/feed.xml',
           description:
             'My personal wikipedia filled with web dev related content, notes and guides.',
-          favicon: `https://www.dante.wiki/icon.png`,
+          favicon: `https://www.dante.wiki/favicon.ico`,
         };
 
         const posts = await $content('posts')
