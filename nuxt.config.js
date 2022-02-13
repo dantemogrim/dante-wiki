ae37714043fa0ec6ceb79a6b4e9f29ca79ca803c
Dante Mogrim <66732473+dantemogrim@users.noreply.github.com>
2022-02-13 19:30:49 +0100

    Favicon RSS issue.

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,11 @@ export default {
 
     link: [
       // Favicon.
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🌀</text></svg>',
+      },
       {
         // Canonical.
         rel: 'icon',
